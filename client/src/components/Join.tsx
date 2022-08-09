9554d6b960169f538b67450e5fd01a7c2d6868ef
codeddave <drizzzydave@gmail.com>
2022-08-10 00:03:10 +0100

    building join page

--- client/src/components/Join.tsx
+++ client/src/components/Join.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Join = () => {
-  return <div>Join</div>;
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  return (
+    <div className="joinOuterContainer">
+      <div className="joinInnerContainer">
+        <h1 className="heading">Join</h1>
+        <div>
+          <input
+            placeholder="Name"
+            className="joinInput"
+            type="text"
+            onChange={}
+          />
+        </div>
+        <div>
+          <input
+            placeholder="Room"
+            className="joinInput mt-20"
+            type="text"
+            onChange={}
+          />
+        </div>
+
+        <Link to={`/chat?name=${name}&room=${room}`}>
+          <button className="button mt-20">Sign In</button>
+        </Link>
+      </div>
+      ;
+    </div>
+  );
 };
 
 export default Join;
