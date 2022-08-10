977d6da0f7f55ce47f7b234315267db5b86587d5
codeddave <drizzzydave@gmail.com>
2022-08-10 02:03:11 +0100

    styling update, change component structure

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "./Chat.css";
 
 const Chat = () => {
   return <div>Chat</div>;
--- client/src/components/Join/Join.tsx
+++ client/src/components/Join/Join.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+import "./Join.css";
 const Join = () => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
@@ -14,7 +15,7 @@ const Join = () => {
             placeholder="Name"
             className="joinInput"
             type="text"
-            onChange={}
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
         <div>
@@ -22,11 +23,14 @@ const Join = () => {
             placeholder="Room"
             className="joinInput mt-20"
             type="text"
-            onChange={}
+            onChange={(e) => setRoom(e.target.value)}
           />
         </div>
 
-        <Link to={`/chat?name=${name}&room=${room}`}>
+        <Link
+          onClick={(e) => (!name || !room ? e.preventDefault() : null)}
+          to={`/chat?name=${name}&room=${room}`}
+        >
           <button className="button mt-20">Sign In</button>
         </Link>
       </div>
