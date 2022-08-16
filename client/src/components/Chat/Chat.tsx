5632893e2fdb85c7d7f2f9d4a7c7a4d87cf38cf7
codeddave <drizzzydave@gmail.com>
2022-08-16 07:43:55 +0100

    setting up socket.io on the chat page

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -1,9 +1,17 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import io from "socket.io-client";
+import { useQuery } from "../../hooks";
 
 import "./Chat.css";
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+let socket;
 const Chat = () => {
+  const name = useQuery().get("name");
+  const room = useQuery().get("room");
+
+  useEffect(() => {
+    socket = io("http://localhost:5000");
+  }, [name, room]);
   return <div>Chat</div>;
 };
 
