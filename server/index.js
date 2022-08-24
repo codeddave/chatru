45c4bafe91556fde11fbe7bae4daf4c11cb83ba7
codeddave <drizzzydave@gmail.com>
2022-08-24 06:29:51 +0100

    fixed CORS error issues

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
-
+const cors = require("cors");
 const router = require("./router");
 
-const PORT = process.env.PORT || 5000;
 const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(cors());
 
 const server = http.createServer(app);
 
-const io = socketio(server);
+const io = socketio(server, { cors: { origin: "*" } });
 
 io.on("connection", (socket) => {
   console.log("We have a new connection");
