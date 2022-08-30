7c37b7a9b5920b87a41043f2f33aef25db67f667
codeddave <drizzzydave@gmail.com>
2022-08-30 04:30:31 +0100

    add user logic

---
+++ users.js
@@ -0,0 +1,25 @@
+const users = [];
+
+const addUser = ({ id, name, room }) => {
+  name = name.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+
+  //prevent user from joining same room using same name as an existing user, prevents user from joining room where username is taken already.
+  const existingUser = users.find(
+    (user) => user.room === room && user.name === name
+  );
+
+  if (existingUser) {
+    return {
+      error: "Username is taken",
+    };
+  }
+
+  const user = { id, name, room };
+  users.push(user);
+  return { user };
+};
+const removeUser = () => {};
+
+const getUser = () => {};
+const getUsersInRoom = () => {};
