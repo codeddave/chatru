9cabd0fc050f4c0685b2212c88a7981adca4b985
codeddave <drizzzydave@gmail.com>
2022-10-02 03:28:17 +0100

    updated Messages component

--- client/src/components/Messages/Messages.tsx
+++ client/src/components/Messages/Messages.tsx
@@ -1,8 +1,23 @@
 import React from "react";
-
+import ScrollToBottom from "react-scroll-to-bottom";
+import Message from "../Message/Message";
+import { MessageType } from "../types";
 import "./Messages.css";
-const Messages = () => {
-  return <div>Messages</div>;
+
+type MessagesProps = {
+  messages: MessageType[];
+  name: string;
+};
+const Messages = ({ messages, name }: MessagesProps) => {
+  return (
+    <ScrollToBottom>
+      {messages.map((message, index) => (
+        <div key={index}>
+          <Message message={message} name={name} />
+        </div>
+      ))}
+    </ScrollToBottom>
+  );
 };
 
 export default Messages;
